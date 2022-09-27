8fb656897adfaddba126406cfe86e07a8825fc2d
Omachi John <omachi.john@.com>
2022-09-27 17:51:58 +0100

    Fetch missions list from Endpoint

--- src/Redux/mission/missionSlice.js
+++ src/Redux/mission/missionSlice.js
@@ -1,3 +1,43 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = [];
+
+export const fetchMissions = createAsyncThunk("mission/fetchMissions", () =>
+  axios
+    .get("https://api.spacexdata.com/v3/missions")
+    .then((response) => response.data)
+);
+
+const missionSlice = createSlice({
+  name: 'mission',
+  initialState,
+  reducers:{
+    join: {
+      reducer: (state, action) => state.map((mission) => (mission.mission_id === action.payload ? {
+        ...mission, reserved: true
+      } : mission)),
+      prepare: (id) =>({
+        payload: id,
+      }),
+    },
+    leave: {
+      reducer: (state, action) => state.map((mission) => (
+        mission.mission_id ===action.payload ? {
+          ...mission, reserved: false
+        } : mission
+      )),
+      prepare: (id) => ({
+        payload: id
+      }),
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled,
+      (state, action) => (action.payload))
+  },
+});
+
+const { join, leave } = missionSlice.actions;
+
+export { fetchMissions, join, leave }
+export default missionSlice.reducer;
